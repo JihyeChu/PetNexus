a9ab549921ffddb38c190e8f7a93b088283b6d35
ChuJH <09strawberry@naver.com>
2023-08-16 18:02:20 +0900

    [Init] git init

---
+++ src/main/java/com/sparta/petnexus/PetNexusApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.petnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetNexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/petnexus/PetNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.petnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
