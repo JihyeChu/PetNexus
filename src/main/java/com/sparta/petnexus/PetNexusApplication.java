9dd85133b7bd48e80689b8db7833f216326e5281
ChuJH <09strawberry@naver.com>
2023-08-17 14:12:53 +0900

    [Chore] jpa implementation + security exclude

--- src/main/java/com/sparta/petnexus/PetNexusApplication.java
+++ src/main/java/com/sparta/petnexus/PetNexusApplication.java
@@ -2,8 +2,9 @@ package com.sparta.petnexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PetNexusApplication {
 
     public static void main(String[] args) {
