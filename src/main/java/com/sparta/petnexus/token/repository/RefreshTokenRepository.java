c026d034f9bdb0e978a57971112330565f6873a6
hojooglim <132051431+hojooglim@users.noreply.github.com>
2023-08-18 16:36:37 +0900

    [Feat] RefreshToken 구현 DB저장 방식 (#10)

---
+++ src/main/java/com/sparta/petnexus/token/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.petnexus.token.repository;
+
+
+import com.sparta.petnexus.token.entity.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByUserId(Long userId);
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
