ad12793c5283909ee5119cba058343727afa5d6c
hyewon218 <won_11111@naver.com>
2023-08-22 00:05:40 +0900

    [Feature] Swagger 추가(JWT 인증 버튼 셋업)

--- src/main/java/com/sparta/petnexus/chat/dto/ChatResponseDto.java
+++ src/main/java/com/sparta/petnexus/chat/dto/ChatResponseDto.java
@@ -1,17 +1,24 @@
 package com.sparta.petnexus.chat.dto;
 
 import com.sparta.petnexus.chat.entity.Chat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@Schema(description = "채팅방 조회 응답 DTO")
 public class ChatResponseDto {
+    @Schema(description = "채팅방 아이디", example = "1")
     private Long roomId;
+    @Schema(description = "작성자 아이디", example = "1")
     private Long userId;
+    @Schema(description = "작성자")
     private String writer;
+    @Schema(description = "메세지 내용")
     private String message;
+    @Schema(description = "메세지 생성 일자")
     private LocalDateTime createdAt;
 
     public static ChatResponseDto of(Chat chat) {
