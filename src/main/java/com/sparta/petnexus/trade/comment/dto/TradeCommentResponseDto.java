d09786061ae4423406a028194836b0d9f15edc1e
ChuJH <09strawberry@naver.com>
2023-08-22 16:50:27 +0900

    [Refactor] Trade Swagger 적용

--- src/main/java/com/sparta/petnexus/trade/comment/dto/TradeCommentResponseDto.java
+++ src/main/java/com/sparta/petnexus/trade/comment/dto/TradeCommentResponseDto.java
@@ -1,13 +1,16 @@
 package com.sparta.petnexus.trade.comment.dto;
 
 import com.sparta.petnexus.trade.comment.entity.TradeComment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@Schema(description = "trade 댓글 응답 DTO")
 public class TradeCommentResponseDto {
 
+    @Schema(description = "trade 품목")
     private String comment;
 
     public static TradeCommentResponseDto of(TradeComment tradeComment){
