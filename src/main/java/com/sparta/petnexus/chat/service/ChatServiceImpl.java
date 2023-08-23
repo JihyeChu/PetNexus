6c4a6d3059dbee8806e3c301c3695aa234403dd7
hyewon218 <won_11111@naver.com>
2023-08-23 12:25:21 +0900

    [Refactor] 에러코드 수정

--- src/main/java/com/sparta/petnexus/chat/service/ChatServiceImpl.java
+++ src/main/java/com/sparta/petnexus/chat/service/ChatServiceImpl.java
@@ -7,8 +7,9 @@ import com.sparta.petnexus.chat.entity.Chat;
 import com.sparta.petnexus.chat.entity.ChatRoom;
 import com.sparta.petnexus.chat.repository.ChatRepository;
 import com.sparta.petnexus.chat.repository.ChatRoomRepository;
+import com.sparta.petnexus.common.exception.BusinessException;
+import com.sparta.petnexus.common.exception.ErrorCode;
 import java.util.List;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ public class ChatServiceImpl implements ChatService {
 
     // 메세지 삭제 - DB Scheduler 적용 필요
     // 방 메세지 목록 조회
+    @Override
     @Transactional(readOnly = true)
     public ChatListResponseDto getAllChatByRoomId(Long roomId) {
         List<Chat> chatList = chatRepository.findAllByChatRoomIdOrderByCreatedAtAsc(roomId);
@@ -30,10 +32,11 @@ public class ChatServiceImpl implements ChatService {
     }
 
     // 메세지 저장
+    @Override
     @Transactional
     public ChatResponseDto saveMessage(ChatRequestDto requestDto) {
         ChatRoom chatRoom = chatRoomRepository.findById(requestDto.getRoomId())
-            .orElseThrow(() -> new NoSuchElementException("해당 채팅방이 존재하지 않습니다 id : " + requestDto.getRoomId()));
+            .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_CHATROOM));
 
         Chat chat = requestDto.toEntity(chatRoom);
         chatRepository.save(chat);
