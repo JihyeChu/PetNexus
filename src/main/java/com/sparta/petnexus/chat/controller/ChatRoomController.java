622f6101f941b7d09f1832b93b705b46fa3d2b27
hyewon218 <won_11111@naver.com>
2023-08-26 18:12:17 +0900

    [Refactor] Builder 패턴 사용, 코드 리팩토링

--- src/main/java/com/sparta/petnexus/chat/controller/ChatRoomController.java
+++ src/main/java/com/sparta/petnexus/chat/controller/ChatRoomController.java
@@ -17,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,9 +24,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/api")
 @Tag(name = "채팅방 관련 API", description = "채팅방 관련 API 입니다.")
 public class ChatRoomController {
@@ -38,9 +38,7 @@ public class ChatRoomController {
     @GetMapping("/openchat")
     @Operation(summary = "오픈채팅방 목록 조회")
     public ResponseEntity<ChatRoomListResponseDto> getOpenChatRooms() {
-        ChatRoomListResponseDto result = chatRoomService.getOpenChatRooms();
-
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok(chatRoomService.getOpenChatRooms());
     }
 
     @PostMapping("/openchat")
@@ -48,11 +46,10 @@ public class ChatRoomController {
     public ResponseEntity<ApiResponse> createOpenChatRoom(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @RequestBody ChatRoomRequestDto requestDto) {
-
         chatRoomService.createOpenChatRoom(requestDto,
             userDetails.getUser());
-
-        return ResponseEntity.ok().body(new ApiResponse("오픈채팅방 생성 성공", HttpStatus.CREATED.value()));
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponse("오픈채팅방 생성 성공", HttpStatus.CREATED.value()));
     }
 
     @PutMapping("/openchat/{id}")
@@ -61,7 +58,6 @@ public class ChatRoomController {
         @Parameter(name = "roomId", description = "특정 채팅방 id", in = ParameterIn.PATH) @PathVariable Long id,
         @RequestBody ChatRoomRequestDto requestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
         chatRoomService.updateOpenChatRoom(id, requestDto,
             userDetails.getUser());
         return ResponseEntity.ok().body(new ApiResponse("오픈채팅방 수정 성공", HttpStatus.OK.value()));
@@ -72,9 +68,7 @@ public class ChatRoomController {
     public ResponseEntity<ApiResponse> deleteChatRoom(
         @Parameter(name = "id", description = "특정 채팅방 id", in = ParameterIn.PATH) @PathVariable Long id,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
         chatRoomService.deleteChatRoom(id, userDetails.getUser());
-
         return ResponseEntity.ok()
             .body(new ApiResponse("채팅방 삭제 성공", HttpStatus.OK.value()));
 
@@ -84,30 +78,24 @@ public class ChatRoomController {
     @Operation(summary = "오픈채팅방 내 채팅 목록 조회", description = "@PathVariable 을 통해 채팅방 id를 받아와, 해당 오픈채팅방에 존재하는 채팅 목록을 조회합니다.")
     public ResponseEntity<ChatListResponseDto> getAllChatByRoomId(
         @Parameter(name = "roomId", description = "특정 채팅방 id", in = ParameterIn.PATH) @PathVariable Long id) {
-        ChatListResponseDto result = chatService.getAllChatByRoomId(id);
-
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(chatService.getAllChatByRoomId(id));
     }
 
     @GetMapping("/tradechat")
     @Operation(summary = "중고거래 채팅방 목록 조회")
     public ResponseEntity<TradeChatRoomListResponseDto> getTradeChatRooms(
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        TradeChatRoomListResponseDto result = chatRoomService.getTradeChatRooms(
-            userDetails.getUser());
-
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok(chatRoomService.getTradeChatRooms(
+            userDetails.getUser()));
     }
 
     @PostMapping("/tradechat/{tradeId}")
     @Operation(summary = "중고거래 채팅방 생성", description = "@PathVariable 을 통해 중고거래 상품 id를 받아와, 해당 중고거래 채팅방을 생성합니다.")
     public ResponseEntity<ApiResponse> createTradeChatRoom(@PathVariable Long tradeId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
         chatRoomService.createTradeChatRoom(tradeId,
             userDetails.getUser());
-
-        return ResponseEntity.ok()
+        return ResponseEntity.status(HttpStatus.CREATED)
             .body(new ApiResponse("중고거래 채팅방 생성 성공", HttpStatus.CREATED.value()));
     }
 
@@ -115,9 +103,7 @@ public class ChatRoomController {
     @Operation(summary = "중고거래 채팅방 내 채팅 목록 조회", description = "@PathVariable 을 통해 채팅방 id를 받아와, 해당 중고거래 채팅방에 존재하는 채팅 목록을 조회합니다.")
     public ResponseEntity<TradeChatListResponseDto> getAllTradeChatByRoomId(
         @Parameter(name = "roomId", description = "특정 채팅방 id", in = ParameterIn.PATH) @PathVariable Long id) {
-        TradeChatListResponseDto result = chatService.getAllTradeChatByRoomId(id);
-
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(chatService.getAllTradeChatByRoomId(id));
     }
 
     @DeleteMapping("/tradechat/{id}")
@@ -125,9 +111,7 @@ public class ChatRoomController {
     public ResponseEntity<ApiResponse> deleteTradeChatRoom(
         @Parameter(name = "id", description = "특정 채팅방 id", in = ParameterIn.PATH) @PathVariable Long id,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
         chatRoomService.deleteTradeChatRoom(id, userDetails.getUser());
-
         return ResponseEntity.ok()
             .body(new ApiResponse("채팅방 삭제 성공", HttpStatus.OK.value()));
     }
